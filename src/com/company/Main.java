b50eefe4a651537a2ac891237889fb6b99efa0e3
tomm8231 <tomm8231@stud.kea.dk>
2022-05-05 10:38:55 +0200

    Amount of items, price pr unit, welcome message working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to THE TOTAL PRICE APPLICATION X-TREME");
+        System.out.println();
+
+        System.out.println("Enter the amount of items:");
+        int amountOfUnits = sc.nextInt();
+
+        System.out.println("Enter the cost of one unit:");
+        int pricePrUnit = sc.nextInt();
+
     }
 }
